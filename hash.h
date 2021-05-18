181c43879acf20edc45d89af9e5fe46395a36c6f
João Tiago Pincho <joao.tiago.pincho@gmail.com>
2021-05-18 02:32:24 +0100

    moved string hash function to its own cpp

--- hash.h
+++ hash.h
@@ -5,13 +5,5 @@
 
 namespace cpplib
 {
-uint64_t hash ( const string &value )
-    {
-    uint64_t result = 0;
-    for ( int iterator = 0; iterator < value.get_length(); ++iterator )
-        {
-        result += ( uint64_t ) pow ( value[iterator], iterator + 1 );
-        }
-    return result;
-    }
+uint64_t hash ( const string &value );
 }
